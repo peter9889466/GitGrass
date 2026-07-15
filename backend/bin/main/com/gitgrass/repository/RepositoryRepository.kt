dec3b7de3275135c139bc224eb51e581580fd269
김승혁 <peter9889466@gmail.com>
2026-07-15 23:32:17 +0900

    feat: 백엔드 Dockerfile 및 docker-compose 설정 파일 추가

--- backend/bin/main/com/gitgrass/repository/RepositoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.gitgrass.repository
-
-import com.gitgrass.domain.Repository
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RepositoryRepository : JpaRepository<Repository, Long> {
-    fun findAllByUserId(userId: Long): List<Repository>
-    fun findAllByUserIdAndIsMonitoredTrue(userId: Long): List<Repository>
-}
--- backend/bin/main/com/gitgrass/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.gitgrass.repository
-
-import com.gitgrass.domain.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByEmail(email: String): User?
-    fun findAllByDiscordAlertActiveTrueAndDiscordAlertTime(discordAlertTime: String): List<User>
-}
